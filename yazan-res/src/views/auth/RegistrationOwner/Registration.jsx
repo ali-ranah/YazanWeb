8a26c0404961a9475b657ce1bd2c1b5143cddc57
ali-ranah <ranasaimali1234@gmail.com>
2024-08-03 17:11:48 +0500

    Notification Updates

---
+++ yazan-res/src/views/auth/RegistrationOwner/Registration.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import axios from "axios";
+import "./Registration.css"; // Importieren der CSS-Datei für die Stildefinitionen
+import RegistrationForm from "./RegistrationForm";
+import registerImage from "../../../assets/register.svg";
+import AxiosRequest from "../../../Components/AxiosRequest";
+
+const RegistrationOwner = () => {
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    password: "",
+    email: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    AxiosRequest
+      .post("/register-client", formData)
+      .then((response) => {
+        if (response.data.status === "ok") {
+          alert("Registration successful");
+          window.location.href = "./";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div className="containerReg" style={{}}>
+      <div className="grid-containerReg">
+        <div className="paperReg">
+          <h3>Create your account!</h3>
+          <RegistrationForm
+            formData={formData}
+            handleInputChange={handleInputChange}
+            handleSubmit={handleSubmit}
+          />
+        </div>
+        <div className="image-containerReg">
+          <img src={registerImage} alt="RegisterReg" className="imgReg" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegistrationOwner;
